27755e73cf435cfe75f8db66749183dd0c8502b6
matt-leyrat <matt.leyrat@gmail.com>
2019-07-09 12:04:06 -0700

    Implement getAllItems query, import Promise, change export format

--- database/index.js
+++ database/index.js
@@ -1,6 +1,20 @@
 const mysql = require('mysql');
 const mysqlConfig = require('./config.js');
+const Promise = require('bluebird');
 
 const connection = mysql.createConnection(mysqlConfig);
 
-module.exports = connection
+const getAllItems = () => {
+  const q = `SELECT * FROM items`;
+  return new Promise ((resolve, reject) => {
+    connection.query(q, (err, results) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(results);
+      }
+    })
+  })
+}
+
+module.exports = { connection, getAllItems };
