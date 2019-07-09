867219f34f49e6971d49a73cc6791b0ecc96f25f
matt-leyrat <matt.leyrat@gmail.com>
2019-07-09 14:47:50 -0700

    Implement GET route for single item

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('../database/index.js');
+const parser = require('body-parser');
 
 const app = express();
 const PORT = 3002;
@@ -17,3 +18,13 @@ app.get('/api/items', (req, res) => {
     .catch(err => console.log('promise rejection error ', err))
   // res.end("successfully sent GET to /api/items");
 });
+
+app.get('/api/:id', (req, res) => {
+  let id = req.params.id
+  console.log(id);
+  db.getSingleItem(id)
+    .then((results) => {
+      res.end(JSON.stringify(results));
+    })
+    .catch(err => console.log('Promise rejection error ', err))
+})
