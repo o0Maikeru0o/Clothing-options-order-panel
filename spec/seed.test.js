50c40bad9eb6f28eda8d4e7e459d8ca980780058
matt-leyrat <matt.leyrat@gmail.com>
2019-07-10 11:02:18 -0700

    Test whether seed script inserts 100 records into items table

---
+++ spec/seed.test.js
@@ -0,0 +1,21 @@
+const db = require('../database/index.js');
+const seed = require('../server/seed.js');
+
+describe('seed script', () => {
+  let items;
+
+  beforeAll(async () => {
+    await db.clearTable();
+    await seed.seed();
+    await db.getAllItems().then(results => items = results);
+  });
+
+  test('Should insert 100 records into items table', () => {
+    expect(items.length).toBe(100);
+  });
+
+  afterAll(async () => {
+    await db.clearTable();
+    await db.connection.end();
+  });
+});
