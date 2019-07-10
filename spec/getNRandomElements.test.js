ea6c4e213d2741f7df592d5e1c08a8a0d57571b2
matt-leyrat <matt.leyrat@gmail.com>
2019-07-10 09:12:22 -0700

    Test whether getNRandomElements returns an array of size n

--- spec/getNRandomElements.test.js
+++ spec/getNRandomElements.test.js
@@ -1,6 +1,10 @@
 const { getNRandomElements } = require('../server/seed.js');
 
-test('Returns an array of size n', () => {
-  const arr = [1, 2, 3, 4, 5, 6, 7]
+test('Returns an array of size n', (done) => {
+  const arr = [1, 2, 3, 4, 5, 6, 7];
   expect(getNRandomElements(arr, 3).length).toBe(3);
-})
+  done();
+  // return getNRandomElements(arr, 3).then((data) => {
+  //   expect(data.length).toBe(3);
+  // })
+});
