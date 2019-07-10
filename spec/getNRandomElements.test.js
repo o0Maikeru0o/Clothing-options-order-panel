bc5f01d5522e7354927634fbab7ba3b23d8c86cd
matt-leyrat <matt.leyrat@gmail.com>
2019-07-10 11:07:22 -0700

    Refactor to use destructuring in import

--- spec/getNRandomElements.test.js
+++ spec/getNRandomElements.test.js
@@ -1,7 +1,7 @@
-const seed = require('../server/seed.js');
+const { getNRandomElements } = require('../server/seed.js');
 
 test('Returns an array of size n', (done) => {
   const arr = [1, 2, 3, 4, 5, 6, 7];
-  expect(seed.getNRandomElements(arr, 3).length).toBe(3);
+  expect(getNRandomElements(arr, 3).length).toBe(3);
   done();
 });
