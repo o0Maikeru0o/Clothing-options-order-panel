29e13affb4bb49cd55e104956d091e9b9a16ea9d
matt-leyrat <matt.leyrat@gmail.com>
2019-07-11 11:17:37 -0700

    Build skeleton for NameAndPrice component

---
+++ client/src/components/Description.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Description = (props) => {
+  return (
+    <div>
+      <h4>Why we made this:</h4>
+      {props.description}
+    </div>
+  )
+};
+
+export default Description;
