c3b634f0289f468c3751b713f2f8dcc1c8e0044b
matt-leyrat <matt.leyrat@gmail.com>
2019-07-11 12:06:57 -0700

    Remove placeholder text

--- client/src/components/NameAndPrice.jsx
+++ client/src/components/NameAndPrice.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const NameAndPrice = (props) => {
   return (
     <div>
-      <div>Name & Price go here: </div> <br></br>
       {props.name} <br></br>
       {props.price}
     </div>
