0aec579cc13348a115d0daa7b7d406f92ffd03c0
matt-leyrat <matt.leyrat@gmail.com>
2019-07-11 17:57:45 -0700

    Render ColorContainer

---
+++ client/src/components/ColorSelector.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {ColorContainer} from '../styling.jsx';
+
+const ColorSelector = (props) => {
+  return (
+    <ColorContainer>color selector goes here</ColorContainer>
+  );
+}
+
+export default ColorSelector;
