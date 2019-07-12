dd0651cfbba299a3c30787b0e99cd1f89135e9fb
matt-leyrat <matt.leyrat@gmail.com>
2019-07-12 14:07:09 -0700

    Stub SizeSelector component

---
+++ client/src/components/SizeSelector.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { SizeContainer } from '../styling.jsx';
+
+const SizeSelector = (props) => {
+  return (
+    <SizeContainer>Size Container Goes Here</SizeContainer>
+  );
+}
+
+export default SizeSelector;
