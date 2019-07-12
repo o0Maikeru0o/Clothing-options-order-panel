7a3690bb977944e4bd732d11259be43a4fe6ce10
matt-leyrat <matt.leyrat@gmail.com>
2019-07-12 14:07:48 -0700

    Adjust render function spacing for clarity

--- client/src/components/ItemSummary.jsx
+++ client/src/components/ItemSummary.jsx
@@ -5,6 +5,7 @@ import NameAndPrice from './NameAndPrice.jsx';
 import Description from './Description.jsx';
 import ColorSelector from './ColorSelector.jsx';
 import Accordions from './Accordions.jsx';
+import SizeSelector from './SizeSelector.jsx';
 import {Main} from '../styling.jsx';
 
 class ItemSummary extends React.Component {
@@ -41,7 +42,9 @@ class ItemSummary extends React.Component {
         <Description
           description={this.state.item.length ? this.state.item[0].description : null}
         />
-        <ColorSelector colors={this.state.item.length ? JSON.parse(this.state.item[0].colors) : []}/>
+        <ColorSelector
+          colors={this.state.item.length ? JSON.parse(this.state.item[0].colors) : []}/>
+        <SizeSelector />
         <Accordions />
       </Main>
     )
