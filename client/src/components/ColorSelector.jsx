dc19af4e489f7ad724bd6e9742f133950aba5fe1
matt-leyrat <matt.leyrat@gmail.com>
2019-07-12 13:36:32 -0700

    render single ColorButton styled-component Working on dynamically rendering a button for each color map is not working (probably due to async behavior from render/componentDidMount)

--- client/src/components/ColorSelector.jsx
+++ client/src/components/ColorSelector.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import ColorButton from './ColorButton.jsx';
 import {ColorContainer} from '../styling.jsx';
 
 const ColorSelector = (props) => {
   return (
     <ColorContainer>
       Color selector goes here
+
+      {/* {props.colors.map((color) => {
+        return <ColorButton color={color}/>
+      })} */}
+
+      <ColorButton colors={props.colors} />
     </ ColorContainer>
   );
 }
