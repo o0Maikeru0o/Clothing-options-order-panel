625499a61574fb67ff2675334ce782e6b6b430af
matt-leyrat <matt.leyrat@gmail.com>
2019-07-12 12:10:01 -0700

    Implement additional styled components, tweak others ColorContainer / AccordionContainer / ItemDescription / WhyWeMadeThis

--- client/src/styling.jsx
+++ client/src/styling.jsx
@@ -5,7 +5,10 @@ export const Main = styled.section`
   background: papayawhip;
   max-width: 500px;
   font-family: Arial, Helvetica, sans-serif;
+  flex-direction: row;
 `;
+
+// Component Containers
 
 export const NamePriceContainer = styled.section`
   padding: 0.5em;
@@ -15,7 +18,22 @@ export const NamePriceContainer = styled.section`
 export const DescriptionContainer = styled.section`
   padding: 0.5em;
   background: white;
+  margin: 0px 0px 30px;
 `;
+
+export const ColorContainer = styled.section`
+  padding: 0.5em;
+  background: white;
+  margin: 0px 0px 30px;
+`;
+
+export const AccordionContainer = styled.div`
+  padding: 0.5em;
+  background: white;
+  margin: 0px 0px 30px;
+`;
+
+// NameAndPrice sub-containers
 
 export const ItemName = styled.div`
   font-size: 2.25rem;
@@ -25,4 +43,16 @@ export const ItemName = styled.div`
 export const ItemPrice = styled.div`
   font-size: 5 rem;
   display: flex;
+  margin: 0px 0px 45px;
+`;
+
+// Description sub-containers
+
+export const ItemDescription = styled.p`
+  margin: 0px 0px 30px;
+`;
+
+export const WhyWeMadeThis = styled.h2`
+  font-size: 1rem;
+  margin: 0px 0px 10px;
 `;
