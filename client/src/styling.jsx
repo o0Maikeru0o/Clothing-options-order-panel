3ac57984fd14015f83a43a043a15bb98ba195ec6
matt-leyrat <matt.leyrat@gmail.com>
2019-07-11 17:09:56 -0700

    Add container components for NameAndPrice and Description

--- client/src/styling.jsx
+++ client/src/styling.jsx
@@ -1,6 +1,17 @@
 import styled from 'styled-components';
 
 export const Main = styled.section`
-  padding: 5em;
+  padding: 1em;
   background: papayawhip;
+  max-width: 300px;
+`;
+
+export const NamePriceContainer = styled.section`
+  padding: 0.5em;
+  background: white;
+`;
+
+export const DescriptionContainer = styled.section`
+  padding: 0.5em;
+  background: white;
 `;
