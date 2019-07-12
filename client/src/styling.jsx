cc8cecdd75a02c4cb9c1ce27982b1e56c9cafe09
matt-leyrat <matt.leyrat@gmail.com>
2019-07-11 17:34:02 -0700

    Export individual styled components for name and price

--- client/src/styling.jsx
+++ client/src/styling.jsx
@@ -3,7 +3,8 @@ import styled from 'styled-components';
 export const Main = styled.section`
   padding: 1em;
   background: papayawhip;
-  max-width: 300px;
+  max-width: 500px;
+  font-family: Arial, Helvetica, sans-serif;
 `;
 
 export const NamePriceContainer = styled.section`
@@ -14,4 +15,14 @@ export const NamePriceContainer = styled.section`
 export const DescriptionContainer = styled.section`
   padding: 0.5em;
   background: white;
+`;
+
+export const ItemName = styled.div`
+  font-size: 2.25rem;
+  font-weight: 600;
+`;
+
+export const ItemPrice = styled.div`
+  font-size: 5 rem;
+  display: flex;
 `;
