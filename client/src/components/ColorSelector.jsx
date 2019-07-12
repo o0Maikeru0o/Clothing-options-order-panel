91aecf54b3985fce56c5fb8140f73b612ded28f8
matt-leyrat <matt.leyrat@gmail.com>
2019-07-12 13:59:11 -0700

    Dynamically render 1 button per color, add key prop

--- client/src/components/ColorSelector.jsx
+++ client/src/components/ColorSelector.jsx
@@ -5,13 +5,11 @@ import {ColorContainer} from '../styling.jsx';
 const ColorSelector = (props) => {
   return (
     <ColorContainer>
-      Color selector goes here
 
-      {/* {props.colors.map((color) => {
-        return <ColorButton color={color}/>
-      })} */}
+      {props.colors.map((color) => {
+        return <ColorButton key={color} color={color}/>
+      })}
 
-      <ColorButton colors={props.colors} />
     </ ColorContainer>
   );
 }
