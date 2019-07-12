e4341151b2d9b43412fd95c96b1485b892b2f95c
matt-leyrat <matt.leyrat@gmail.com>
2019-07-12 13:35:23 -0700

    Render styled components for RadioButton and RadioButtonLabel

---
+++ client/src/components/ColorButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { RadioButton, RadioButtonLabel } from '../styling.jsx';
+
+const ColorButton = (props) => {
+  return (
+    <div>
+      <RadioButton />
+      <RadioButtonLabel />
+    </div>
+  );
+};
+
+export default ColorButton;
