3347559d9dc502cbaea71f30b91098a1bed843e8
matt-leyrat <matt.leyrat@gmail.com>
2019-07-13 11:46:40 -0700

    Implement DropDownList and DropDownItem styling, tweak others

--- client/src/styling.jsx
+++ client/src/styling.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Main = styled.section`
   padding: 1em;
-  background: papayawhip;
+  background: rgb(250,250,250);
   max-width: 500px;
   font-family: Arial, Helvetica, sans-serif;
   flex-direction: row;
@@ -13,33 +13,65 @@ export const Main = styled.section`
 export const NamePriceContainer = styled.div`
   padding: 0.5em;
   background: white;
+  background: rgb(250,250,250)
 `;
 
 export const DescriptionContainer = styled.div`
   padding: 0.5em;
-  background: white;
-  margin: 0px 0px 30px;
+  background: rgb(250,250,250);
+  margin: 0px;
 `;
 
 export const ColorContainer = styled.div`
+  background: rgb(250,250,250);
   display: flex;
   flex-direction: row;
+  flex-wrap: wrap;
   padding: 0.5em;
   background: white;
   margin: 0px 0px 30px;
+  border-top: 1px solid rgba(0, 0, 0, .3);
 `;
 
 export const SizeContainer = styled.div`
+  height: 40px;
   display: flex;
   padding: 0.5em;
   background: white;
   margin: 0px 0px 30px;
+  border: 1px solid rgba(0, 0, 0, .1);
+  box-shadow: box-shadow: 0px 0px 0px 8px rgba(0,0,0,0.5);
 `;
 
 export const AccordionContainer = styled.div`
   padding: 0.5em;
   background: white;
   margin: 0px 0px 30px;
+`;
+
+// DropDown components
+export const DropDownList = styled.ul`
+  display: flex;
+  z-index: 1;
+  flex-direction: column;
+  padding: 0;
+  margin: 5px;
+  position: relative;
+  visibility: ${(props) => {
+    return props.dropDownOpen ? 'visible' : 'hidden';
+  }}
+`;
+
+export const DropDownItem = styled.li`
+  display: block;
+  z-index: 2;
+  transition-duration: 0.2s;
+  height: 40px;
+  width: 100%;
+  padding: 0.5em;
+  background: white;
+  text-align: center;
+  box-shadow: box-shadow: 0px 0px 0px 8px rgba(0,0,0,0.5);
 `;
 
 // NameAndPrice sub-containers
@@ -50,7 +82,7 @@ export const ItemName = styled.div`
 `;
 
 export const ItemPrice = styled.div`
-  font-size: 5 rem;
+  font-size: 7 rem;
   display: flex;
   margin: 0px 0px 45px;
 `;
@@ -81,7 +113,7 @@ export const RadioButton = styled.button`
   margin: 5px;
 `;
 
-export const RadioButtonLabel = styled.label`
+export const RadioButtonLabel = styled.div`
   height: auto;
   width: auto;
   font-weight: 300;
