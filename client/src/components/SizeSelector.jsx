7e99b4c06494443b59d8128299a637cc0668639c
matt-leyrat <matt.leyrat@gmail.com>
2019-07-12 17:23:14 -0700

    Render SizeDropDown component, pass down sizes

--- client/src/components/SizeSelector.jsx
+++ client/src/components/SizeSelector.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import SizeDropDown from './SizeDropDown.jsx';
 import { SizeContainer } from '../styling.jsx';
 
 const SizeSelector = (props) => {
   return (
-    <SizeContainer>Size Container Goes Here</SizeContainer>
+    <SizeContainer>
+      <SizeDropDown sizes={props.sizes}/>
+    </SizeContainer>
   );
 }
 
