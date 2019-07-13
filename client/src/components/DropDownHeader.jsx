ae5841e0d9abafd4463f0f18d71515b5c37d1e1a
matt-leyrat <matt.leyrat@gmail.com>
2019-07-13 11:44:08 -0700

    Add styling to DropdownHeader

--- client/src/components/DropDownHeader.jsx
+++ client/src/components/DropDownHeader.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
+// import { DropDownHeaderContainer } from '../styling.jsx';
+import styled from 'styled-components';
 
 const DropDownHeader = (props) => {
   return (
-    <div>Drop Down Header here</div>
+    <div
+    onClick={props.toggleDropDown}
+    className={props.className}
+    >
+      Select Size
+    </div>
   );
 };
 
-export default DropDownHeader;
+export default styled(DropDownHeader)`
+  // border: 2px solid red;
+  width: 100%;
+  height: 100%;
+`;
+
+// export default DropDownHeader;
