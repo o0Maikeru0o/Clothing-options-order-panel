4e23c66520b3b188da066a4feda619e8827e0c81
matt-leyrat <matt.leyrat@gmail.com>
2019-07-15 16:52:02 -0700

    Adjust styling

--- client/src/components/DropDownHeader.jsx
+++ client/src/components/DropDownHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SelectedSize, DropDownLabel } from '../styling.jsx';
 // import { DropDownHeaderContainer } from '../styling.jsx';
 import styled from 'styled-components';
 
@@ -8,15 +9,21 @@ const DropDownHeader = (props) => {
     onClick={props.toggleDropDown}
     className={props.className}
     >
-      Select Size
+      <DropDownLabel>Size</DropDownLabel>
+
+      <SelectedSize>{props.selectedSize} ▽ </SelectedSize>
     </div>
   );
 };
 
 export default styled(DropDownHeader)`
   // border: 2px solid red;
+  display: flex;
   width: 100%;
   height: 100%;
+  :hover {
+    cursor: pointer;
+  }
 `;
 
 // export default DropDownHeader;
