2c19f78573901a8447e845a2323dcca6603fa988
matt-leyrat <matt.leyrat@gmail.com>
2019-07-15 10:44:34 -0700

    Enclose currency tag in its own component for styling

--- client/src/components/NameAndPrice.jsx
+++ client/src/components/NameAndPrice.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import {NamePriceContainer, ItemName, ItemPrice} from '../styling.jsx';
+import {NamePriceContainer, ItemName, ItemPrice, CurrencyTag} from '../styling.jsx';
 
 const NameAndPrice = (props) => {
   return (
     <NamePriceContainer>
       <ItemName>{props.name}</ItemName>
       <br></br>
-      <ItemPrice>{props.price} USD</ItemPrice>
+      <ItemPrice>
+        {props.price}
+        <CurrencyTag>USD</CurrencyTag>
+      </ItemPrice>
+
     </NamePriceContainer>
   )
 };
