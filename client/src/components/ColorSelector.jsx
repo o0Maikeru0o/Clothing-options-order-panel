3cf1aa3886e85235a5e0fe062115297673ae44da
matt-leyrat <matt.leyrat@gmail.com>
2019-07-15 11:23:37 -0700

    add styled component for ColorContainer to ColorSelector file

--- client/src/components/ColorSelector.jsx
+++ client/src/components/ColorSelector.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import ColorButton from './ColorButton.jsx';
-import {ColorContainer, RadioButtonLabel} from '../styling.jsx';
+import {RadioButtonLabel} from '../styling.jsx';
 import styled from 'styled-components';
+
+const ColorContainer = styled.div`
+  background: rgb(250,250,250);
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 0.5em;
+  background: white;
+  margin: 0px 0px 30px;
+  border-top: 1px solid rgba(0, 0, 0, .3);
+`;
 
 const ColorSelector = (props) => {
   return (
     <ColorContainer className={props.className}>
 
       {props.colors.map((color) => {
-        return <ColorButton key={color} color={color} selectColor={props.selectColor}/>
+        return <ColorButton
+        key={color}
+        color={color}
+        selectColor={props.selectColor}
+        selectedColor={props.selectedColor}
+        />
       })}
 
-      <RadioButtonLabel>{props.selectedColor}</RadioButtonLabel>
+      <RadioButtonLabel>{props.selectedColor.charAt(0).toUpperCase() + props.selectedColor.slice(1)}</RadioButtonLabel>
 
     </ ColorContainer>
   );
