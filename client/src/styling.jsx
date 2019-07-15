70d97a065cf21bbdd2abaa9dd07567daced5313e
matt-leyrat <matt.leyrat@gmail.com>
2019-07-15 16:57:27 -0700

    Tweak several components, move others to their respective files

--- client/src/styling.jsx
+++ client/src/styling.jsx
@@ -1,18 +1,18 @@
 import styled from 'styled-components';
 
 export const Main = styled.section`
+  display: flex;
+  flex-direction: column;
   padding: 1em;
   background: rgb(250,250,250);
   max-width: 500px;
   font-family: Arial, Helvetica, sans-serif;
-  flex-direction: row;
 `;
 
-// Component Containers
+// ///////// Component Containers ///////////////////
 
 export const NamePriceContainer = styled.div`
   padding: 0.5em;
-  background: white;
   background: rgb(250,250,250)
 `;
 
@@ -22,41 +22,25 @@ export const DescriptionContainer = styled.div`
   margin: 0px;
 `;
 
-export const ColorContainer = styled.div`
-  background: rgb(250,250,250);
-  display: flex;
-  flex-direction: row;
-  flex-wrap: wrap;
-  padding: 0.5em;
-  background: white;
-  margin: 0px 0px 30px;
-  border-top: 1px solid rgba(0, 0, 0, .3);
-`;
-
 export const SizeContainer = styled.div`
   height: 40px;
   display: flex;
-  padding: 0.5em;
+  padding: 0;
   background: white;
   margin: 0px 0px 30px;
   border: 1px solid rgba(0, 0, 0, .1);
-  box-shadow: box-shadow: 0px 0px 0px 8px rgba(0,0,0,0.5);
 `;
 
-export const AccordionContainer = styled.div`
-  padding: 0.5em;
-  background: white;
-  margin: 0px 0px 30px;
-`;
+// ///////// DropDown components ///////////////////////
 
-// DropDown components
 export const DropDownList = styled.ul`
   display: flex;
   z-index: 1;
   flex-direction: column;
   padding: 0;
-  margin: 5px;
+  margin: 0px;
   position: relative;
+  border: 1px solid rgba(0, 0, 0, .1);
   visibility: ${(props) => {
     return props.dropDownOpen ? 'visible' : 'hidden';
   }}
@@ -68,13 +52,27 @@ export const DropDownItem = styled.li`
   transition-duration: 0.2s;
   height: 40px;
   width: 100%;
-  padding: 0.5em;
+  padding-top: 1em;
   background: white;
   text-align: center;
-  box-shadow: box-shadow: 0px 0px 0px 8px rgba(0,0,0,0.5);
+
+  :hover {
+    cursor: pointer;
+    background: rgba(250, 250, 250, 1);
+  }
 `;
 
-// NameAndPrice sub-containers
+export const DropDownLabel = styled.div`
+  margin-top: 10px;
+  margin-left: 20px;
+`;
+
+export const SelectedSize = styled.div`
+  margin-left: 69%;
+  margin-top: 8px;
+`;
+
+// //////////// NameAndPrice sub-containers ///////////////////
 
 export const ItemName = styled.div`
   font-size: 2.25rem;
@@ -82,12 +80,18 @@ export const ItemName = styled.div`
 `;
 
 export const ItemPrice = styled.div`
-  font-size: 7 rem;
+  font-size: 1rem;
+  font-weight: 100;
   display: flex;
   margin: 0px 0px 45px;
 `;
 
-// Description sub-containers
+export const CurrencyTag = styled.div`
+  font-size: .75rem;
+  padding: .75% 1% 0px;
+`;
+
+// ////////////// Description sub-containers ///////////////////////
 
 export const ItemDescription = styled.p`
   margin: 0px 0px 30px;
@@ -96,27 +100,4 @@ export const ItemDescription = styled.p`
 export const WhyWeMadeThis = styled.h2`
   font-size: 1rem;
   margin: 0px 0px 10px;
-`;
-
-
-// Color Picker Buttons
-export const RadioButtonContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin: 0px 0px 0px;
-`;
-
-export const RadioButton = styled.button`
-  background: ${props => props.value};
-  height: 1.5rem;
-  width: 3rem;
-  margin: 5px;
-`;
-
-export const RadioButtonLabel = styled.div`
-  height: auto;
-  width: auto;
-  font-weight: 300;
-  line-height: 1.25;
-  margin: 0 auto 1rem;
 `;
