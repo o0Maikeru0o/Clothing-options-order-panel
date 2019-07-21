1fbd41e6f99f2dd462775da8f9880a3fba5476b2
matt-leyrat <matt.leyrat@gmail.com>
2019-07-20 21:02:10 -0700

    Render greyed out color buttons based on stock for selected size

--- client/src/components/ColorButton.jsx
+++ client/src/components/ColorButton.jsx
@@ -29,34 +29,36 @@ const RadioButtonContainer = styled.div`
   }};
 `;
 
+const GreyedOutContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-right: 12px
+  padding: .055rem;
+  border: ${(props) => {
+    return props.color === props.selectedColor.colorName ? '1.3px solid black' : '';
+  }};
+  background-image: ${(props) => {
+    return 'repeating-linear-gradient(150deg, transparent 0%, transparent 48%, black 50%, transparent 52%, transparent 100%)'
+  }};
+`;
+
 const RadioButton = styled.button`
   background: ${props => props.value};
   height: 1.5rem;
   width: 3rem;
   margin: 2px;
 
-  opacity: ${(props) => {
-    if (props.selectedColor.sizes.length) {
-      let checkStock = (colorObj, size) => {
-        return colorObj.sizes.filter((sizeObj) => {return sizeObj.size == size})[0].stock;
-      };
-      let capitalizedColor = props.value.charAt(0).toUpperCase() + props.value.slice(1);
-
-      if (capitalizedColor === props.selectedColor.colorName) {
-        if (checkStock(props.selectedColor, props.selectedSize) === 0) {
-          return '0.5';
-        } else {
-          return '1';
-        }
-      }
-    } else {
-      return '1';
-    }
-  }};
-
   :hover {
     cursor: pointer;
   }
+`;
+
+const GreyedOutButton = styled.button`
+  background: ${props => props.value};
+  height: 1.5rem;
+  width: 3rem;
+  margin: 2px;
+  opacity: 0.5;
 `;
 
 const RadioButtonLabel = styled.div`
@@ -68,17 +70,43 @@ const RadioButtonLabel = styled.div`
 `;
 
 const ColorButton = (props) => {
-  return (
-    <RadioButtonContainer selectedSize={props.selectedSize} selectedColor={props.selectedColor} color={props.color}>
-      <RadioButton
-      color={props.color}
-      value={props.color.toLowerCase()}
-      onClick={props.selectColor}
-      selectedColor={props.selectedColor}
-      selectedSize={props.selectedSize}
-      />
-    </RadioButtonContainer>
-  );
+
+  let checkStock = (colorObj, size) => {
+    return colorObj.sizes.filter((sizeObj) => { return sizeObj.size == size })[0].stock;
+  };
+  const findObjByColorName = (array, color) => {
+    return array.filter((colorObj) => { return colorObj.colorName === color})
+  }
+  const relatedObj = findObjByColorName(props.colors, props.color)[0];
+  const selectedSize = props.selectedSize
+
+  if (checkStock(relatedObj, props.selectedSize) === 0) {
+    return (
+      <GreyedOutContainer selectedSize={props.selectedSize} selectedColor={props.selectedColor} color={props.color}>
+        <GreyedOutButton
+        colors={props.colors}
+        color={props.color}
+        value={props.color.toLowerCase()}
+        onClick={props.selectColor}
+        selectedColor={props.selectedColor}
+        selectedSize={props.selectedSize}
+        />
+      </GreyedOutContainer>
+    );
+  } else {
+    return (
+      <RadioButtonContainer selectedSize={props.selectedSize} selectedColor={props.selectedColor} color={props.color}>
+        <RadioButton
+        colors={props.colors}
+        color={props.color}
+        value={props.color.toLowerCase()}
+        onClick={props.selectColor}
+        selectedColor={props.selectedColor}
+        selectedSize={props.selectedSize}
+        />
+      </RadioButtonContainer>
+    );
+  }
 };
 
 export default ColorButton;
