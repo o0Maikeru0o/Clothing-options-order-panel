41df834d9d562929966069d287c971b847d4dc54
o0Maikeru0o <mnickandros@gmail.com>
2019-07-25 10:25:53 -0400

    CRUD functions created

--- database/index.js
+++ database/index.js
@@ -1,3 +1,73 @@
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize('postgres://localhost:5432/wawa_melon');
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+const { Model } = Sequelize;
+
+class Item extends Model {}
+Item.init({
+  id: {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: Sequelize.STRING,
+  fabric: Sequelize.STRING,
+  care: Sequelize.STRING,
+  features: Sequelize.STRING,
+  colors: Sequelize.STRING,
+  prize: Sequelize.STRING,
+},
+// options
+{
+  sequelize,
+  modelName: 'item',
+});
+
+const getAllItems = () => Item.findAll();
+
+const getItemById = id => Item.findOne({ where: { id } })
+  .then(item => item)
+  .catch(err => console.log(err));
+
+const getItemByName = name => Item.findOne({ where: { name } })
+  .then(item => item)
+  .catch(err => console.log(err));
+
+const createItem = item => Item.findOrCreate({ where: { name: item.name } })
+  .spread((newItem, created) => newItem)
+  .catch(err => console.log(err));
+
+const updateItem = (id, params) => Item.Update({ params }, { where: { id } })
+  .then(updated => updated)
+  .catch(err => console.log(err));
+
+const deleteItem = id => Item.destroy({ where: id })
+  .then(destroyed => destroyed)
+  .catch(err => console.log(err));
+
+module.exports = {
+  getAllItems,
+  getItemById,
+  getItemByName,
+  createItem,
+  updateItem,
+  deleteItem,
+};
+
 // const mysql = require('mysql');
 // const Promise = require('bluebird');
 // const mysqlConfig = require('./config.js');
@@ -43,59 +113,3 @@
 //     });
 //   });
 // };
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('postgres://localhost:5432/wawa_melon');
-
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-
-const { Model } = Sequelize;
-
-class Item extends Model {}
-Item.init({
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
-  name: Sequelize.STRING,
-  description: Sequelize.STRING,
-  fabric: Sequelize.STRING,
-  care: Sequelize.STRING,
-  features: Sequelize.STRING,
-  colors: Sequelize.STRING,
-  prize: Sequelize.STRING,
-},
-// options
-{
-  sequelize,
-  modelName: 'item',
-});
-
-const getAllItems = () => Item.findAll();
-
-const getItemById = id => Item.findOne({ where: { id } }).then(item => item).catch(err => console.log(err));
-
-const getItemByName = name => Item.findOne({ where: { name } }).then(item => item).catch(err => console.log(err));
-
-const createItem = item => Item.findOrCreate({ where: { name: item.name } }).spread((item, created) => item).catch(err => console.log(err));
-
-const updateItem = (id, params) => Item.Update({ params }, { where: { id } }).then(updated => updated).catch(err => console.log(err));
-
-const deleteItem = id => Item.destroy({ where: id }).then(destroyed => destroyed).catch(err => console.log(err));
-
-module.exports = {
-  getAllItems,
-  getItemById,
-  getItemByName,
-  createItem,
-  updateItem,
-  deleteItem,
-};
